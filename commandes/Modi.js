e574f9111e229b8fb152994d81969e4f55e206b2
Popkid <popkidtechs@gmail.com>
2024-10-29 08:11:43 +0300

    ⭕POP KID⭕

--- commandes/Modi.js
+++ commandes/Modi.js
@@ -48,7 +48,7 @@ zokou({
 ⦿ *Direct YtLink:* ${videoUrl}
 
 ╭────────────────◆
-│ *_𝗥𝗲𝗴𝗮𝗿𝗱𝘀 𝐏𝐎𝐏𝐊𝐈𝐃❤️._*
+│ *_𝗥𝗲𝗴𝗮𝗿𝗱𝘀 POPKID TECH🌟._*
 ╰─────────────────◆`
         };
 
@@ -107,7 +107,7 @@ zokou({
         // Prepare the message with song details
         const infoMess = {
           image: { url: videos[0].thumbnail },
-          caption: `*POPKID-MD SONG PLAYER*\n
+          caption: `*CHARITY-MD SONG PLAYER*\n
 ╭───────────────◆
 │ *Title:* ${videos[0].title}
 │ *Quality:* mp3 (320kbps)
@@ -118,7 +118,7 @@ zokou({
 ╰────────────────◆
 ⦿ *Direct YtLink:* ${videoUrl}
 ╭────────────────◆
-│ *_𝗥𝗲𝗴𝗮𝗿𝗱𝘀 ̼𝐏𝐎𝐏𝐊𝐈𝐃❤️._*
+│ *_𝗥𝗲𝗴𝗮𝗿𝗱𝘀 𝗖̼𝗔̼𝗦̼𝗘̼𝗬̼𝗥̼𝗛̼𝗢̼𝗗̼𝗘̼𝗦̼._*
 ╰─────────────────◆`
         };
 
@@ -131,7 +131,7 @@ zokou({
           mimetype: 'audio/mp4'
         }, { quoted: ms });
        
-        repondre('*Popkid md has just downloaded your song*...');
+        repondre('*Charity md has just downloaded your song*...');
       } else {
         repondre('Failed to download audio. Please try again later.');
       }
@@ -170,4 +170,120 @@ zokou({
 
       // Call the API endpoint with the video URL to fetch the video download URL
       const apiResponse = await fetch(`${BaseUrl}/api/download/ytmp4?url=${encodeURIComponent(videoUrl)}&apikey=${giftedapikey}`);
-      const apiResult = 
+      const apiResult = await apiResponse.json();
+
+      if (apiResult.status === 200 && apiResult.success) {
+        const videoDlUrl = apiResult.result.download_url;
+
+        // Prepare the message with video details
+        const infoMess = {
+          image: { url: videos[0].thumbnail },
+          caption: `*CHARITY-MD VIDEO PLAYER*\n
+╭───────────────◆
+│ *Title:* ${videos[0].title}
+│ *Quality:* 720p-HD
+│ *Duration:* ${videos[0].timestamp}
+│ *Viewers:* ${videos[0].views}
+│ *Uploaded:* ${videos[0].ago}
+│ *Artist:* ${videos[0].author.name}
+╰────────────────◆
+⦿ *Direct YtLink:* ${videoUrl}
+╭────────────────◆
+│ *_𝗥𝗲𝗴𝗮𝗿𝗱𝘀 𝗖̼𝗔̼𝗦̼𝗘̼𝗬̼𝗥̼𝗛̼𝗢̼𝗗̼𝗘̼𝗦̼._*
+╰─────────────────◆`
+        };
+
+        // Send video details
+        await zk.sendMessage(dest, infoMess, { quoted: ms });
+
+        // Send the video as a URL (direct download link)
+        await zk.sendMessage(dest, {
+          video: { url: videoDlUrl },
+      caption: "*𝐆𝐄𝐍𝐄𝐑𝐀𝐓𝐄𝐃 𝐁𝐘 𝐂𝐇𝐀𝐑𝐈𝐓𝐘*",
+          mimetype: 'video/mp4'
+        }, { quoted: ms });
+
+        repondre('*Charity md has just downloaded your video*...');
+      } else {
+        repondre('Failed to download the video. Please try again later.');
+      }
+    } else {
+      repondre('No videos found.');
+    }
+  } catch (error) {
+    console.error('Error from API:', error);
+    repondre('An error occurred while searching or downloading the video.' + error);
+  }
+});
+
+zokou({
+  nomCom: "videodoc",
+  categorie: "Download",
+  reaction: "🎥"
+}, async (dest, zk, commandeOptions) => {
+  const { ms, repondre, arg } = commandeOptions;
+
+  if (!arg[0]) {
+    repondre("Please insert a song/video name.");
+    return;
+  }
+
+  try {
+    let topo = arg.join(" ");
+    let videos = [];
+
+    // Perform YouTube search
+    const search = await yts(topo);
+    videos = search.videos;
+
+    if (videos && videos.length > 0) {
+      const videoUrl = videos[0].url;
+
+      // Call the API endpoint with the video URL to fetch the video download URL
+      const apiResponse = await fetch(`${BaseUrl}/api/download/ytmp4?url=${encodeURIComponent(videoUrl)}&apikey=${giftedapikey}`);
+      const apiResult = await apiResponse.json();
+
+      if (apiResult.status === 200 && apiResult.success) {
+        const videoDlUrl = apiResult.result.download_url;
+
+        // Prepare the message with video details
+        const infoMess = {
+          image: { url: videos[0].thumbnail },
+          caption: `*CHARITY-MD VIDEO PLAYER*\n
+╭───────────────◆
+│ *Title:* ${videos[0].title}
+│ *Quality:* 720p-HD
+│ *Duration:* ${videos[0].timestamp}
+│ *Viewers:* ${videos[0].views}
+│ *Uploaded:* ${videos[0].ago}
+│ *Artist:* ${videos[0].author.name}
+╰────────────────◆
+
+⦿ *Direct YtLink:* ${videoUrl}
+╭────────────────◆
+│ *_𝗥𝗲𝗴𝗮𝗿𝗱𝘀 𝗖̼𝗔̼𝗦̼𝗘̼𝗬̼𝗥̼𝗛̼𝗢̼𝗗̼𝗘̼𝗦̼🌟 ._*
+╰─────────────────◆`
+        };
+
+        // Send video details
+        await zk.sendMessage(dest, infoMess, { quoted: ms });
+
+        // Send the video as a URL (direct download link)
+        await zk.sendMessage(dest, {
+          document: { url: videoDlUrl },
+               caption: "*𝐆𝐄𝐍𝐄𝐑𝐀𝐓𝐄𝐃 𝐁𝐘 𝐂𝐇𝐀𝐑𝐈𝐓𝐘*",
+          mimetype: 'video/mp4'
+        }, { quoted: ms });
+ 
+       repondre('*Charity md has just downloaded your video*...');
+      } else {
+        repondre('Failed to download the video. Please try again later.');
+      }
+    } else {
+      repondre('No videos found.');
+    }
+  } catch (error) {
+    console.error('Error from API:', error);
+    repondre('An error occurred while searching or downloading the video.' + error);
+  }
+});
